b4abddf07a4822852ce10eb128b4931a0d14d909
flagosa <f.lagosa@udd.cl>
2024-07-29 00:28:17 -0400

    train_model.py - analysis.ipynb modificados y borrado de random_forest.joblib anterior

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,10 +1,13 @@
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+import os
 
 def train_model(df, features, target_col):
     X = df[features]
     y = df[target_col]
     model = RandomForestClassifier(n_estimators=100, max_depth=10)
     model.fit(X, y)
+    
+    os.makedirs('models', exist_ok=True)
     joblib.dump(model, 'models/random_forest.joblib')
     return model

13e513c59ea30a73dc76ef4ca312b7005c4d1250
flagosa <f.lagosa@udd.cl>
2024-07-29 00:37:55 -0400

    cambios en train_model.py especificando ruta de model - eliminacion de guardar model desde notebook

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,6 +8,6 @@ def train_model(df, features, target_col):
     model = RandomForestClassifier(n_estimators=100, max_depth=10)
     model.fit(X, y)
     
-    os.makedirs('models', exist_ok=True)
-    joblib.dump(model, 'models/random_forest.joblib')
+    os.makedirs('../models', exist_ok=True)
+    joblib.dump(model, '../models/random_forest.joblib')
     return model
